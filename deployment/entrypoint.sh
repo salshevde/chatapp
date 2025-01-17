1cd111b926e57a85463fc7862b94c9b1c1190630
Saloni Shevde <132834991+salshevde@users.noreply.github.com>
2025-01-17 22:16:42 +0530

    rm db

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Wait for postgres
-while ! nc -z db 5432; do
-    echo "Waiting for postgres..."
-    sleep 1
-done
-
-echo "PostgreSQL started"
-
 # Apply database migrations
 python manage.py migrate --noinput
 
