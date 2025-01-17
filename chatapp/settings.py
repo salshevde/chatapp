62a3f6157e740026ab54caa306bc7d28bde06452
Saloni Shevde <132834991+salshevde@users.noreply.github.com>
2025-01-17 22:26:40 +0530

    static fies

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -133,6 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
